2ee10f6dd5c9a77dcfc7b3f627acf7abb16e8b06
kathryngraysonnanz <nanz@c02f810emd6v.attlocal.net>
2025-05-12 16:27:26 -0400

    added basic manual add form – TO-DO: add validation, masking for ISBN input, explore whether it's possible to check for duplication

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import styles from "./page.module.scss";
 
-import { use, useState } from "react";
+import { useState } from "react";
 
 import { saveMediaToFirestore } from "../../lib/firestore";
 import {BarcodeScanner, getBookInfo} from "./scanner";
@@ -11,7 +11,7 @@ import Header from "./header";
 import { Button } from "@progress/kendo-react-buttons";
 import { Card } from "@progress/kendo-react-layout";
 import { Dialog, DialogActionsBar } from "@progress/kendo-react-dialogs";
-import { Input, NumericTextBox, MaskedTextBox } from "@progress/kendo-react-inputs";
+import { MaskedTextBox, Input } from "@progress/kendo-react-inputs";
 
 
 export default function Home() {
@@ -20,6 +20,17 @@ export default function Home() {
     const [scannerOpen, setScannerOpen] = useState(false);
     const [isbnSearchOpen, setIsbnSearchOpen] = useState(false);
     const [manualIsbn, setManualISBN] = useState(); 
+    const [manualAddOpen, setManualAddOpen] = useState(false);
+    const [formData, setFormData] = useState({
+      added: new Date(),
+      author: "",
+      isbn: "",
+      loaned: false,
+      published: "", 
+      title: "",
+      type: "book", 
+
+    });
 
     function resetScanner() {
       setMediaInfo(null);
@@ -29,6 +40,19 @@ export default function Home() {
     function cancelScan() {
       setMediaInfo(null);
       setScannerOpen(false);
+      setIsbnSearchOpen(false);
+      setManualISBN(null);
+      setManualAddOpen(false);
+      setFormData({
+        added: new Date(),
+        author: "",
+        isbn: "",
+        loaned: false,
+        published: "", 
+        title: "",
+        type: "book", 
+
+      })
     }
 
     function formatMedia(mediaInfo){
@@ -50,6 +74,19 @@ export default function Home() {
       setMediaInfo(result); 
     };
 
+    const handleChange = (field) => (e) => {
+      setFormData({ ...formData, [field]: e.target.value });
+    };
+
+    const handleSave = async () => {
+      try {
+        await saveMediaToFirestore({ ...formData, duplicate: false });
+        cancelScan();
+      } catch (err) {
+        console.error("Error saving manual entry:", err);
+      }
+    };
+
 
   return (
     <div>
@@ -62,21 +99,33 @@ export default function Home() {
       <>
         <Button onClick={(e) => setScannerOpen(!scannerOpen)}>Scan Barcode</Button>
         <Button onClick={(e) => setIsbnSearchOpen(!isbnSearchOpen)}>Search by ISBN</Button>
-        <Button>Manual Add</Button>
+        <Button onClick={(e) => setManualAddOpen(!manualAddOpen)}>Manually Add</Button>
       </>
       }
 
       {!mediaInfo && isbnSearchOpen && 
-        <Dialog title={'ISBN Search'}>
+        <Dialog title={'ISBN Search'} onClose={(e) => cancelScan()}>
            <MaskedTextBox mask="0-000000-00-0" onChange={(e) => setManualISBN(e.value.replace(/-/g, ""))}/>
           <DialogActionsBar>
             <Button onClick={(e) => manualISBNSearch(manualIsbn.value)}>Search</Button>
-            <Button>Cancel</Button>
+            <Button onClick={(e) => cancelScan()}>Cancel</Button>
           </DialogActionsBar>
         </Dialog>
       }
 
-      {console.log(manualIsbn)}
+      {!mediaInfo && manualAddOpen && 
+        <Dialog title={'Manually Add Media Information'} onClose={(e) => cancelScan()}>
+           <Card type="warning">Note that manually added media is not checked for duplication in the database. <br/> <b>The barcode scan or ISBN search input methods are recommended whenever possible.</b></Card>
+            <Input label="Title" value={formData.title} onChange={handleChange("title")} />
+            <Input label="Author(s)" value={formData.author} onChange={handleChange("author")} />
+            <Input label="Publish Date (optional)" value={formData.published} onChange={handleChange("published")} />
+            <Input label="ISBN (optional)" value={formData.isbn} onChange={handleChange("isbn")} />
+          <DialogActionsBar>
+            <Button onClick={handleSave}>Save</Button>
+            <Button onClick={(e) => cancelScan()}>Cancel</Button>
+          </DialogActionsBar>
+        </Dialog>
+      }
 
       {!mediaInfo && scannerOpen && 
         <>
