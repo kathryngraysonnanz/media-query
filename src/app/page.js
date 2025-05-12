435eb008a8793f961940bd5008f47d32ad7e6dd8
kathryngraysonnanz <nanz@c02f810emd6v.attlocal.net>
2025-05-12 15:57:12 -0400

    fixed bug where scanner was logging barcode number as ISBN; now even searches that use the barcode will save the returned ISBN number from the OpenLib API search. This will prevent issues where a book that was photo scanned and later manually input would (incorrectly) not flag as the same

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,24 @@
 
 import styles from "./page.module.scss";
 
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../lib/firebase";
+import { use, useState } from "react";
+
 import { saveMediaToFirestore } from "../../lib/firestore";
-
-import { useState } from "react";
-
-import BarcodeScanner from "./scanner";
+import {BarcodeScanner, getBookInfo} from "./scanner";
 import Header from "./header";
 
 import { Button } from "@progress/kendo-react-buttons";
 import { Card } from "@progress/kendo-react-layout";
+import { Dialog, DialogActionsBar } from "@progress/kendo-react-dialogs";
+import { Input, NumericTextBox, MaskedTextBox } from "@progress/kendo-react-inputs";
 
 
 export default function Home() {
 
     const [mediaInfo, setMediaInfo] = useState(null);
     const [scannerOpen, setScannerOpen] = useState(false);
+    const [isbnSearchOpen, setIsbnSearchOpen] = useState(false);
+    const [manualIsbn, setManualISBN] = useState(); 
 
     function resetScanner() {
       setMediaInfo(null);
@@ -44,54 +45,75 @@ export default function Home() {
       saveMediaToFirestore(savedMedia);
     }
 
+    const manualISBNSearch = async () => {
+      const result = await getBookInfo(manualIsbn);
+      setMediaInfo(result); 
+    };
+
 
   return (
     <div>
       <Header></Header>
       <main> 
-        <h1>The Grayson-Nanz Media Library</h1>
+        <h1>Media Library</h1>
         <div style={{ padding: "1rem" }}>
       
       {!scannerOpen && 
       <>
-        <h2 style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>Scan a Barcode</h2>
-        <Button onClick={(e) => setScannerOpen(!scannerOpen)}>Open the Scanner</Button>
+        <Button onClick={(e) => setScannerOpen(!scannerOpen)}>Scan Barcode</Button>
+        <Button onClick={(e) => setIsbnSearchOpen(!isbnSearchOpen)}>Search by ISBN</Button>
+        <Button>Manual Add</Button>
       </>
-       
       }
+
+      {!mediaInfo && isbnSearchOpen && 
+        <Dialog title={'ISBN Search'}>
+           <MaskedTextBox mask="0-000000-00-0" onChange={(e) => setManualISBN(e.value.replace(/-/g, ""))}/>
+          <DialogActionsBar>
+            <Button onClick={(e) => manualISBNSearch(manualIsbn.value)}>Search</Button>
+            <Button>Cancel</Button>
+          </DialogActionsBar>
+        </Dialog>
+      }
+
+      {console.log(manualIsbn)}
+
       {!mediaInfo && scannerOpen && 
-        <BarcodeScanner onScan={(scanned) => setMediaInfo(scanned)} />
+        <>
+          <BarcodeScanner onScan={(scanned) => setMediaInfo(scanned)} />
+          <Button onClick={(e) => cancelScan()}>Cancel</Button>
+        </>
       }
+
       {mediaInfo && 
-        <div>
+        <>
           {mediaInfo.error && 
             <>
               <p>Error: {mediaInfo.error}</p>
               <Button onClick={(e) => resetScanner()}>Rescan</Button>
+              <Button onClick={(e) => cancelScan()}>Cancel</Button>
             </>
           }
           {!mediaInfo.error && 
-          <div className={styles.results}>
-            <h2>Scan Results</h2>
-            <br/>
-            {mediaInfo.duplicate && 
-              <Card type="warning"> Note: this title is already in your library.</Card>
-            }
-            <img src={mediaInfo.cover} className={styles.cover}/>
-            <p><b>ISBN:</b> {mediaInfo.isbn}</p>
-            <p><b>Title:</b> <i>{mediaInfo.title}</i></p>
-            <p><b>Author(s):</b> {mediaInfo.authors}</p>
-            <p><b>Publish Date:</b> {mediaInfo.publish_date}</p>
-
-            <Button onClick={(e) => resetScanner()}>Rescan</Button>
-            <Button>Edit Information</Button>
-            <Button disabled={mediaInfo.duplicate} onClick={(e) => formatMedia(mediaInfo)}>Add to Library</Button>
-            <Button onClick={(e) => cancelScan()}>Cancel</Button>
-          
-            </div>
+            <Dialog title={'Scan Results'}>
+              {console.log(mediaInfo)}
+              {mediaInfo.duplicate && 
+                <Card type="warning"> This title is already in your library.</Card>
+              }
+              <img src={mediaInfo.cover} className={styles.cover}/>
+              <p><b>ISBN:</b> {mediaInfo.isbn}</p>
+              <p style={{textTransform: 'capitalize'}}><b>Title:</b> <i>{mediaInfo.title}</i></p>
+              <p><b>Author(s):</b> {mediaInfo.authors}</p>
+              <p><b>Publish Date:</b> {mediaInfo.publish_date}</p>
+              <DialogActionsBar>
+                  <Button onClick={(e) => resetScanner()}>Rescan</Button>
+                  <Button>Edit Information</Button>
+                  <Button disabled={mediaInfo.duplicate} onClick={(e) => formatMedia(mediaInfo)}>Add to Library</Button>
+                  <Button onClick={(e) => cancelScan()}>Cancel</Button>
+              </DialogActionsBar>
+            </Dialog>
           }
-          {console.log('info', mediaInfo)}
-        </div>
+        </>
       }
      
     </div>
--- src/app/scanner.js
+++ src/app/scanner.js
@@ -5,38 +5,40 @@ import Quagga from "quagga";
 
 import { checkMediaExists } from "../../lib/firestore";
 
-const BarcodeScanner = ({ onScan }) => {
-  const scannerRef = useRef(null);
+export const getBookInfo = async (isbn) => {
+  console.log('isbn:', isbn)
+  const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  const getBookInfo = async (isbn) => {
-    const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`;
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const isDuplicate = await checkMediaExists(isbn);
+    if (data[`ISBN:${isbn}`]) {
+      const book = data[`ISBN:${isbn}`];
 
-      if (data[`ISBN:${isbn}`]) {
-        const book = data[`ISBN:${isbn}`];
-      
-        const bookDetails = {
-          isbn: isbn, 
-          title: book.title,
-          authors: book.authors ? book.authors.map((author) => author.name).join(", ") : "Unknown",
-          publish_date: book.publish_date || "Unknown",
-          cover: book.cover ? book.cover.large : null,
-          description: book.notes || "No description available",
-          duplicate: isDuplicate,
-        };
-        onScan(bookDetails);
-      } else {
-        console.error("Book not found. ISBN: " + isbn);
-        onScan({error: "Book not found. ISBN: " + isbn})
-      }
-    } catch (error) {
-      console.error("Error fetching book info:", error);
-      onScan({error: "Error fetching book info"})
+      let searchedISBN = book.identifiers.isbn_10[0]
+      const isDuplicate = await checkMediaExists(searchedISBN);
+      console.log('book', book)
+
+      return {
+        isbn: searchedISBN,
+        title: book.title,
+        authors: book.authors ? book.authors.map((author) => author.name).join(", ") : "Unknown",
+        publish_date: book.publish_date || "Unknown",
+        cover: book.cover ? book.cover.large : null,
+        description: book.notes || "No description available",
+        duplicate: isDuplicate,
+      };
+    } else {
+      return { error: `Book not found. ISBN: ${isbn}` };
     }
-  };
+  } catch (error) {
+    console.error("Error fetching book info:", error);
+    return { error: "Error fetching book info" };
+  }
+};
+
+export const BarcodeScanner = ({ onScan }) => {
+  const scannerRef = useRef(null);
 
   useEffect(() => {
     if (!scannerRef.current) return;
@@ -72,11 +74,13 @@ const BarcodeScanner = ({ onScan }) => {
       Quagga.start();
     });
 
-    Quagga.onDetected((result) => {
+    Quagga.onDetected(async (result) => {
       const isbn = result.codeResult.code;
       console.log("Detected ISBN: ", isbn);
       Quagga.stop();
-      getBookInfo(isbn);
+
+      const bookDetails = await getBookInfo(isbn);
+      onScan(bookDetails);
     });
 
     return () => {
@@ -91,4 +95,3 @@ const BarcodeScanner = ({ onScan }) => {
   );
 };
 
-export default BarcodeScanner;
