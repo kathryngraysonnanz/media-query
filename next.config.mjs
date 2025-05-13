2a81e8d6d64af617e319051ff34bb4c768db1c49
kathryngraysonnanz <nanz@c02f810emd6v.attlocal.net>
2025-05-13 16:43:40 -0400

    github pages setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/media-query', 
+  assetPrefix: '/media-query/', 
+};
 
 export default nextConfig;
