cc6cf55b92bc2ea6ac35ca28005eed9cf38c3855
kathryngraysonnanz <kathrynkaygrayson@gmail.com>
2025-08-19 16:50:25 -0400

    updated scanner method

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -11,9 +11,9 @@ export default function Header() {
             <h1>Media Library</h1>
         </AppBarSection>
         <AppBarSection>
-            <a href="/">Add</a>
-            <a href="/library">Search</a>
-            <a href="/browse">Browse</a>
+            <a href="https://kathryngraysonnanz.github.io/media-query/">Add</a>
+            <a href="https://kathryngraysonnanz.github.io/media-query/library">Search</a>
+            <a href="https://kathryngraysonnanz.github.io/media-query//browse">Browse</a>
         </AppBarSection>
     </AppBar>
     )
